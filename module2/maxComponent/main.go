4dd0f7e4318dec595ce08b86658bc118fd1b46a3
darleet <darleet@mail.ru>
2023-08-17 02:29:18 +0300

    Small fixes

--- module2/maxComponent/main.go
+++ module2/maxComponent/main.go
@@ -12,64 +12,63 @@ const (
 	BLACK
 )
 
-// Стек
+// stack - стек
 type stack struct {
 	data []int
 }
 
-// Вершина графа
+// node - вершина графа
 type node struct {
 	color int
 	isRed bool
 	links []int
 }
 
-// Добавление в стек
+// Push добавляет элемент в стек
 func (s *stack) Push(n int) {
 	s.data = append(s.data, n)
 }
 
-// Удаление из стека
+// Pop удаляет элемент из стека
 func (s *stack) Pop() int {
 	n := s.data[len(s.data)-1]
 	s.data = s.data[:len(s.data)-1]
 	return n
 }
 
-// Проверка стека на пустоту
+// IsEmpty проверяет стек на пустоту
 func (s *stack) IsEmpty() bool {
 	return len(s.data) == 0
 }
 
-// Инициализация стека
+// initStack инициализирует стек
 func initStack() *stack {
 	return &stack{data: make([]int, 0)}
 }
 
-// Добавляет индекс следующей вершины
+// Add добавляет индекс следующей вершины
 func (n *node) Add(destIndex int) {
 	n.links = append(n.links, destIndex)
 	n.color = WHITE
 }
 
 // Читает ребра из Stdin
-func readEdge(reader *bufio.Reader) (int, int) {
-	input, _ := reader.ReadString('\n')
-	input = strings.Trim(input, "\n")
-	nodes := strings.Split(input, " ")
+func readEdge(scanner *bufio.Scanner) (int, int) {
+	scanner.Scan()
+	nodes := strings.Fields(scanner.Text())
 	nodeIndexA, _ := strconv.Atoi(nodes[0])
 	nodeIndexB, _ := strconv.Atoi(nodes[1])
 	return nodeIndexA, nodeIndexB
 }
 
-// Окрашивает все вершины в белый
+// clearGraph окрашивает все вершины в белый
 func clearGraph(nodeList []*node) {
 	for _, element := range nodeList {
 		element.color = WHITE
 	}
 }
 
-// Проходит по компоненте, возвращает кол-во вершин и ребер
+// analyzeComponent проходит по компоненте, возвращает кол-во вершин и ребер
 func analyzeComponent(startNodeIndex int, nodeList []*node) (int, int) {
 	nodeStack := initStack()
 	var nodeCounter, edgeCounter int
@@ -92,7 +91,7 @@ func analyzeComponent(startNodeIndex int, nodeList []*node) (int, int) {
 	return nodeCounter, edgeCounter
 }
 
-// Функция для нахождения максимальной компоненты
+// findMaxComponent находит максимальную компоненту
 func findMaxComponent(nodeList []*node) int {
 	var maxNodes, maxEdges, maxComponentIndex int
 	for startNodeIndex := range nodeList {
@@ -111,11 +110,10 @@ func findMaxComponent(nodeList []*node) int {
 	return maxComponentIndex
 }
 
-// Функция для вывода компоненты
+// printGraph выводит компоненту
 func printGraph(nodeList []*node, maxComponentIndex int, writer *bufio.Writer) {
 	nodeStack := initStack()
 	_, _ = writer.WriteString("graph {\n")
-
 	for startNodeIndex := range nodeList {
 		if nodeList[startNodeIndex].color == WHITE {
 			nodeStack.Push(startNodeIndex)
@@ -123,18 +121,15 @@ func printGraph(nodeList []*node, maxComponentIndex int, writer *bufio.Writer) {
 				currentNodeIndex := nodeStack.Pop()
 				if nodeList[currentNodeIndex].color == WHITE {
 					nodeList[currentNodeIndex].color = BLACK
-
 					// выведем точку
 					_, _ = writer.WriteString(strconv.Itoa(currentNodeIndex))
 					if startNodeIndex == maxComponentIndex {
 						_, _ = writer.WriteString(" [color=\"red\"]")
 					}
 					_, _ = writer.WriteString("\n")
-
 					for _, otherNodeIndex := range nodeList[currentNodeIndex].links {
 						if nodeList[otherNodeIndex].color == WHITE {
 							nodeStack.Push(otherNodeIndex)
-
 							// выведем ребро
 							_, _ = writer.WriteString(strconv.Itoa(currentNodeIndex) + " -- " +
 								strconv.Itoa(otherNodeIndex))
@@ -142,32 +137,26 @@ func printGraph(nodeList []*node, maxComponentIndex int, writer *bufio.Writer) {
 								_, _ = writer.WriteString(" [color=\"red\"]")
 							}
 							_, _ = writer.WriteString("\n")
-
 						}
 					}
-
 				}
 			}
 		}
 	}
-
 	_, _ = writer.WriteString("}")
 }
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 	defer func() {
 		_ = writer.Flush()
 	}()
 
-	input, _ := reader.ReadString('\n')
-	input = strings.Trim(input, "\n")
-	nodeNum, _ := strconv.Atoi(input)
-
-	input, _ = reader.ReadString('\n')
-	input = strings.Trim(input, "\n")
-	edgeNum, _ := strconv.Atoi(input)
+	scanner.Scan()
+	nodeNum, _ := strconv.Atoi(scanner.Text())
+	scanner.Scan()
+	edgeNum, _ := strconv.Atoi(scanner.Text())
 
 	// Список инцидентности
 	nodeList := make([]*node, 0)
@@ -176,7 +165,7 @@ func main() {
 	}
 
 	for i := 0; i < edgeNum; i++ {
-		nodeIndexA, nodeIndexB := readEdge(reader)
+		nodeIndexA, nodeIndexB := readEdge(scanner)
 		nodeList[nodeIndexA].Add(nodeIndexB)
 		nodeList[nodeIndexB].Add(nodeIndexA)
 	}
